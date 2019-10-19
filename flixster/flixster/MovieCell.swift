8adc9443ded22847545c98f1416f422058ad2d94
dnagaraj10 <31592319+dnagaraj10@users.noreply.github.com>
2019-10-18 21:36:44 -0700

    Add files via upload

---
+++ flixster/flixster/MovieCell.swift
@@ -0,0 +1,18 @@
+//
+//  MovieCell.swift
+//  flixster
+//
+//  Created by Divya Nagaraj on 10/18/19.
+//  Copyright © 2019 divyanagaraj. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MovieCell: UITableViewCell {
+    
+    
+    override func awakeFromNib() {
+        <#code#>
+    }
+}
